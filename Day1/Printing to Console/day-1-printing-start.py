a106cac6a4d3fe880e6991f5b47ad7612617080f
Maktumhusen Challamarad <44393494+Maktumhusen123@users.noreply.github.com>
2023-08-19 21:17:00 +0530

    Update day-1-printing-start.py

--- Day1/Printing to Console/day-1-printing-start.py
+++ Day1/Printing to Console/day-1-printing-start.py
@@ -1,2 +1,7 @@
+#print() function is used to print the message
 print("Welcome to Python Bootcamp")
 print("Hello World!")
+
+# + is used to concatenate strings
+print("This is" + " a simple " + "program.")
+
