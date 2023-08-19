63718503b475ae8afad2bcdda5f4fab02a204e4d
Maktumhusen Challamarad <44393494+Maktumhusen123@users.noreply.github.com>
2023-08-19 21:50:25 +0530

    Update day-1-printing-start.py

--- Day1/Printing to Console/day-1-printing-start.py
+++ Day1/Printing to Console/day-1-printing-start.py
@@ -5,3 +5,14 @@ print("Hello World!")
 # + is used to concatenate strings
 print("This is" + " a simple " + "program.")
 
+# input() function is used to take input from the user
+print("Hello " + input("What is your name"))
+
+# len() is used to get length of string
+print(len(input("What is your favourite pet?")))
+
+
+# variables are used as container for storing values
+name = input("What is your Name: ")
+age = input("What is your Age: ")
+print("Your name is " + name + " and You are " + age + " Years old.")
