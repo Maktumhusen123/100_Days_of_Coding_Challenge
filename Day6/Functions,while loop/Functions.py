22afc62b7c78bfbf2766f22b0963839e6e8e20d2
Maktumhusen Challamarad <44393494+Maktumhusen123@users.noreply.github.com>
2023-08-24 23:29:00 +0530

    Add files via upload

---
+++ Day6/Functions,while loop/Functions.py
@@ -0,0 +1,10 @@
+# Defining Functions
+def my_function():
+    print("This is a function")
+
+# Calling Functions
+my_function()
+
+# you can call function as many times as you want
+for i in range(6):
+    my_function()
