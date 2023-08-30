9b47724967595d2f3170f4f96b29dde76aed7666
Maktumhusen Challamarad <44393494+Maktumhusen123@users.noreply.github.com>
2023-08-30 10:57:22 +0530

    Create functions.py

---
+++ Day8/Functions with Inputs/functions.py
@@ -0,0 +1,17 @@
+# simple function
+def greet():
+    print("Hello")
+    print("Welcome")
+    print("Good evening")
+
+# Function that allows for input
+def greet_with_parameters(name,language):               # name and language are parameter
+    print(f"Hello {name}")
+    print(f"Welcomet to {language} programming")
+    print("Good evening")
+
+# calling simple function
+greet()
+
+# Calling function with Parameters
+greet_with_parameters("Maktum","Python")            # Maktum and Python are arguments passed to the function
