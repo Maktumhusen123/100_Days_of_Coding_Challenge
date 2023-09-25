25d69377cffc07049549e76776b0aa333ecaf532
Maktumhusen Challamarad <44393494+Maktumhusen123@users.noreply.github.com>
2023-09-25 12:10:09 +0530

    Create EvaluateLines.py

---
+++ Day13/Debugging/EvaluateLines.py
@@ -0,0 +1,13 @@
+year = int("What is your year of birth: ")
+if year > 1980 and year < 1994:
+    print("you are milennial")
+elif year > 1994:
+    print("You are Gen Z")
+
+# In this program what if user enter 1980 or 1994, Here we need to evaluate each line and fix this.
+year = int("What is your year of birth: ")
+if year >= 1980 and year <= 1994:
+    print("you are milennial")
+elif year > 1994:
+    print("You are Gen Z")
+
