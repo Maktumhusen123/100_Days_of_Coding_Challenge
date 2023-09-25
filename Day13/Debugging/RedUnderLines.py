8a86502e62ae2d2ad58f29801b9e6263224b19d9
Maktumhusen Challamarad <44393494+Maktumhusen123@users.noreply.github.com>
2023-09-25 12:36:43 +0530

    Create RedUnderLines.py

---
+++ Day13/Debugging/RedUnderLines.py
@@ -0,0 +1,8 @@
+name = input("What is your name: ")
+if len(name) > 5:
+print(f"Your Name is {name}")
+
+# you can see red line indicates that something is wrong, you can hover on it and fix the error.
+
+if len(name) > 5:
+    print(f"Your name is {name}")
