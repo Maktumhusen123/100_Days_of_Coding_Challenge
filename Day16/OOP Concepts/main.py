313da4e56379f989bbfee1a7b5b1dc9be7d2c341
Maktumhusen Challamarad <44393494+Maktumhusen123@users.noreply.github.com>
2023-09-27 11:20:17 +0530

    Update main.py

--- Day16/OOP Concepts/main.py
+++ Day16/OOP Concepts/main.py
@@ -4,8 +4,9 @@ my_turtle = Turtle()  # object = Class() here my_turtle is an object of class Tu
 print(my_turtle)
 my_turtle.shape("square")
 my_turtle.color("green")
+my_turtle.forward(100)
 
 screen = Screen()   # screen object is created
-print(screen.canvheight)  # canvheight is an attribute of Screen() class
-screen.exitonclick()    # exitonclick() is a method of Screen() class
+print(screen.canvheight)  # canvheight is an attribute of Screen() class, objectname.attribute
+screen.exitonclick()    # exitonclick() is a method of Screen() class, objectname.method()
 
