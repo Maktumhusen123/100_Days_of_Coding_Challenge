426b33f036911840ea572abf862cc2257c7904ec
Maktumhusen Challamarad <44393494+Maktumhusen123@users.noreply.github.com>
2023-09-27 10:18:34 +0530

    Add files via upload

---
+++ Day 14/Higher Lower Game/higherlower.py
@@ -0,0 +1,27 @@
+from game_data import data
+
+import random
+
+print("-----------------------------Welcome to Higher Lower Game-----------------------------")
+
+# Generate random account from data
+account_A = random.choice(data)
+account_B = random.choice(data)
+print(account_A["name"])
+print(account_B["name"])
+# Format account data into printable format
+
+# Ask user to guess
+
+# Check if user is correct
+    # Get follower count and use if statement to check if user is correct
+
+# Give user feedback on their guess
+
+# score keeping
+
+# make game repeatable.
+
+#  move account B to A.
+
+# clears the screen between rounds.
