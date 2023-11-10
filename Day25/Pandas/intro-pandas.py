3e2337fac3127f997919ad0053c9c2e62e7bd3d5
Maktumhusen Challamarad <44393494+Maktumhusen123@users.noreply.github.com>
2023-11-10 11:37:09 +0530

    Update intro-pandas.py

--- Day25/Pandas/intro-pandas.py
+++ Day25/Pandas/intro-pandas.py
@@ -1,1 +1,21 @@
 with open('weather_data.csv') as data_file:
+  data = data_file.readlines()
+  print(data)
+
+import csv
+with open('weather_data.csv') as data_file:
+  data = csv.reader(data_file)
+  days = []
+  temperatures = []
+  conditions = []
+  for row in data:
+    temperatures.append(row[1])
+    days.append(row[0])
+    conditions.append(row[2]
+
+
+import pandas
+
+data =  pandas.read_csv("weather_data.csv")
+print(data)
+print(data["temp"]
