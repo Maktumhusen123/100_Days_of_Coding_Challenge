ef00404164d5bf8353fcb24f710ecf7350659af8
Maktumhusen Challamarad <44393494+Maktumhusen123@users.noreply.github.com>
2023-11-13 08:30:06 +0530

    Update intro-pandas.py

--- Day25/Pandas/intro-pandas.py
+++ Day25/Pandas/intro-pandas.py
@@ -1,22 +1,29 @@
-with open('weather_data.csv') as data_file:
-  data = data_file.readlines()
-  print(data)
+# Traditional way of handling data in files
+# with open("weather-data.csv") as data_file:
+#    data = data_file.readlines()
+#    print(data)
 
-import csv
-with open('weather_data.csv') as data_file:
-  data = csv.reader(data_file)
-  days = []
-  temperatures = []
-  conditions = []
-  for row in data:
-    temperatures.append(row[1])
-    days.append(row[0])
-    conditions.append(row[2]
+# Using csv library
+# import csv
+#
+# with open("weather-data.csv") as data_file:
+#    data = csv.reader(data_file)
+#    print(data)
+#    days = []
+#    temp = []
+#    cond = []
+#    for row in data:
+#        if row[0] != "Days":
+#            days.append(row[0])
+#        if row[1] != "Temperature":
+#            temp.append(row[1])
+#        if row[2] != "Condition":
+#            cond.append(row[2])
+# print(days)
 
-
+# Using pandas library
 import pandas
 
-data =  pandas.read_csv("weather_data.csv")
-print(type(data) # Pandas DataFrame
-print(data)
-print(type(data["temp"])) #Pandas Series
+data = pandas.read_csv("weather-data.csv")
+print(data)  # pandas DataFrame
+print(data["Temperature"])  # pandas Series
