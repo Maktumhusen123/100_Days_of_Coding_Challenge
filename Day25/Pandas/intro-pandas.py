f8fad8ac31d86cfde47823e14fabb2de4da2d569
Maktumhusen Challamarad <44393494+Maktumhusen123@users.noreply.github.com>
2023-11-13 09:19:27 +0530

    Update intro-pandas.py

--- Day25/Pandas/intro-pandas.py
+++ Day25/Pandas/intro-pandas.py
@@ -27,6 +27,25 @@ import pandas
 data = pandas.read_csv("weather-data.csv")
 # print(data)  # pandas DataFrame
 # print(data["Temperature"])  # pandas Series
-temp_list = data["Temperature"].to_list()
-avgTemp = sum(temp_list)/len(temp_list)
-print(f"Average of Temperatures is {avgTemp}")
+
+# temp_list = data["Temperature"].to_list()
+# avgTemp = sum(temp_list)/len(temp_list)
+# print(f"Average of Temperatures is {avgTemp}")
+# or you can use Series methods
+# print(data["Temperature"].mean())
+# print(data["Temperature"].max())
+# print(data["Temperature"].min())
+
+# Get data in row with filtering
+# print(data[data.Temperature == data.Temperature.max()])
+monday = data[data.Days == "Monday"]
+print(monday.Temperature)
+
+# Create a DataFrame from scratch
+data_dict = {
+    "students": ["Maktum", "Shahid", "Hanip"],
+    "scores": [78, 77, 77]
+}
+new_data = pandas.DataFrame(data_dict)
+print(new_data)
+new_data.to_csv("student_data.csv")
