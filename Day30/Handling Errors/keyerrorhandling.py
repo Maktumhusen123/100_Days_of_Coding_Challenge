888dd3a28ab66082587211e244fc892ff5544215
Maktumhusen Challamarad <44393494+Maktumhusen123@users.noreply.github.com>
2023-11-17 09:46:55 +0530

    Update keyerrorhandling.py

--- Day30/Handling Errors/keyerrorhandling.py
+++ Day30/Handling Errors/keyerrorhandling.py
@@ -1,9 +1,10 @@
 facebook_posts = eval(input())
 
 total_likes = 0
-try:
-    total_likes = {post['Likes'] for post in facebook_posts}
-except KeyError as e:
-    print(f"the key {e} does not exist")
+for post in facebook_posts:
+    try:
+        total_likes += post['Likes']
+    except KeyError as e:
+        print(f"the key {e} does not exist")
 
 print(total_likes)
